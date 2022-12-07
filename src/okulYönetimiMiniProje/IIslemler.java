56ec838f557a5ea0c506cca82495cf620d2f3ee1
Nurullah Demir <nurullah.demir1453@gmail.com>
2022-12-07 12:33:25 +0300

    okul yönetimi proje sonu

---
+++ src/okulYönetimiMiniProje/IIslemler.java
@@ -0,0 +1,17 @@
+package okulYönetimiMiniProje;
+
+public interface IIslemler {
+    /*
+    ============= İŞLEMLER =============
+		 1-EKLEME
+		 2-ARAMA
+		 3-LİSTELEME
+		 4-SİLME
+		 Q-ÇIKIŞ
+     */
+    public void ekleme();
+    public void arama();
+    public void listeleme();
+    public void silme();
+    public void cikis();
+}
---
+++ src/okulYönetimiMiniProje/Runner.java
@@ -0,0 +1,8 @@
+package okulYönetimiMiniProje;
+
+public class Runner {
+    public static void main(String[] args) {
+        AnaMenu anaMenu=new AnaMenu();
+        anaMenu.anaMenu();
+    }
+}
